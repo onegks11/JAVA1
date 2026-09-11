e730f9ffcd09f59f95af1718d67dfea5034c402c
onegks111 <dnjsgks111@gmail.com>
2026-09-11 13:10:00 +0900

    test

---
+++ Week02/sample02-1/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+    첫번째 실습 내용
+ */
+void main() {
+    String name = "이순신";//문자열 데이터를 선언하는 선언문(대입문)
+
+    System.out.printf("이름: %s\n", name);
+    System.out.print("대한민국\n");
+    System.out.println("경복대학교");
+    System.out.println("유한");
+}
